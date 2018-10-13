66128644ec11ba653e70496e7e93ca9a4657c722
unknown <andrewparise1994@gmail.com>
2018-10-13 19:53:24 -0400

    Andrew Parise - fixed Main.elm to work with 0.19

--- README.md
+++ README.md
@@ -2,8 +2,30 @@
 A sample project to test out and become more familiar with the Elm programming language syntax.
 
 
-# Backend API
+# Commands
+
+## `yarn api`
+Starts a fake Back-End Server on `localhost:4000`.
+
+## `yarn build`
+Creates a webpack build, and creates a distributable bundle in `dist`.
+
+## `yarn client`
+Runs the webpack dev server.
+
+## `yarn clean`
+Cleans the `dist` directory.
+
+
+# Webpack
+
+## Versions
+Webpack v2.2
+Elm-Webpack-Loader v4.2
+
+
+# API
 
 ## Running Locally
-Execute from the project root: `npm run start-backend`.
+Execute from the project root: `npm run api`, or `yarn api`.
 
---
+++ src/Main.elm
@@ -0,0 +1,69 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (src)
+
+
+-- MODEL
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "Hello", Cmd.none )
+
+
+
+-- MESSAGES
+
+
+type Msg
+    = NoOp
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text model ]
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = subscriptions
+        }
